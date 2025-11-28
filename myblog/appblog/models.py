754879e9657547acaa40b30b98d8c5514ab9d838
Jojogeek <ptavangar1382@gmail.com>
2025-11-28 20:09:32 +0330

    categories preview added to home and detailed post page

--- myblog/appblog/models.py
+++ myblog/appblog/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def str(self):
+        return self.name
+
+
+
 class Tag(models.Model):
     name = models.CharField(max_length=50,unique=True)
 
@@ -16,7 +24,7 @@ class Post(models.Model):
     image = models.ImageField(upload_to='post_images/', blank=True,null=True)
     tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='posts')
-
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.title

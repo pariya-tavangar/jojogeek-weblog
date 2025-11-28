7328212cfdc8ac1e82bbcf22cc48111e3e2687fc
Jojogeek <ptavangar1382@gmail.com>
2025-11-28 19:07:35 +0330

    previous next button fixed

--- myblog/appblog/views.py
+++ myblog/appblog/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils.text import slugify
+from django.core.paginator import Paginator
 
 
 def home(request):
     posts = Post.objects.all().order_by('-create_at')[:2]
     recent_posts = Post.objects.all().order_by('-create_at')[:4]
-    return render(request,"home.html",{'posts':posts , 'recent_posts':recent_posts})
+    post_list = Post.objects.all().order_by('-create_at')
+    paginator = Paginator(post_list, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request,"home.html",{'posts':posts , 'recent_posts':recent_posts,'page_obj':page_obj})
 
 
 def post_detail(request,  title):

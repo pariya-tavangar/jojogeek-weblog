2efd13f0ebac84ea301e68e50712b3034c2beaf8
Jojogeek <ptavangar1382@gmail.com>
2025-11-30 19:12:19 +0330

    no comments submits

--- myblog/appblog/models.py
+++ myblog/appblog/models.py
@@ -30,3 +30,22 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=100)
+    body = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    parent = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        related_name='replies',
+        on_delete=models.CASCADE
+    )  # so you can reply to comments
+
+    def str(self):
+        return f'Comment by {self.name}'
